a0b1f0cf1b910f7abc2cb43e9a2fcf05ba8394a9
唐文文 <18365284289@163.com>
2019-02-12 10:54:47 +0800

    init

---
+++ src/main/java/com/fate/jktest/JktestApplication.java
@@ -0,0 +1,14 @@
+package com.fate.jktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JktestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JktestApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/fate/jktest/JktestApplicationTests.java
@@ -0,0 +1,17 @@
+package com.fate.jktest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JktestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
